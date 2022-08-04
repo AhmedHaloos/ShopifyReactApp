c3a90466c330446b468a4c14a7f1bfc3d142e425
AhmedHaloos <as0720871@gmail.com>
2022-08-04 14:21:52 +0200

    make api work with react app

--- src/App.js
+++ src/App.js
@@ -5,68 +5,91 @@ import axios from 'axios';
 
 const shop = 'iti-ism';
 const version = "2022-07";
-const resource = "customers";
+const resource = "products";
 const token = "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";//iti-ism
 const apiKey = "f9435203660033c2fe73a34c23ffd4dd";// iti-ism
 const passsword = "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"//iti-ism
 
-function getAllCustomers() {
+// GET /admin/api/2022-01/products.json undefined
+// Host: iti-ism.myshopify.com
+// User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0
+// Accept: application/json, text/plain, */*
+// Accept-Language: en-US,en;q=0.5
+// Accept-Encoding: gzip, deflate, br
+// X-Shopify-Access-Token: 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
+// Referer: http://localhost:3000/
 
-  const apiUrl = `https://${apiKey}:${passsword}@${shop}.myshopify.com/admin/api/2022-01/${resource}.json`
-  // const apiUrl = `https://jsonplaceholder.typicode.com/todos`
-  return axios(
-    {
-      method: 'get',
-      url: apiUrl,
-      headers: {
-        "content-type": "application/json, charset=utf-8",
-        'X-Shopify-Access-Token': `${token}`,
-       
-        // 'Authorization': apiKey
+// GET /admin/api/2022-01/products.json HTTP/3
+// Host: iti-ism.myshopify.com
+// User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0
+// Accept: application/json, text/plain, */*
+// Accept-Language: en-US,en;q=0.5
+// Accept-Encoding: gzip, deflate, br
+// X-Shopify-Access-Token: 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
+// Connection: keep-alive
+// Referer: http://localhost:3000/
+// Sec-Fetch-Dest: empty
+// Sec-Fetch-Mode: no-cors
+// Sec-Fetch-Site: cross-site
+// Pragma: no-cache
+// Cache-Control: no-cache
 
-      },
 
-    })
-}
+// function getAllCustomers() {
+
+//   const apiUrl = `https://${apiKey}:${passsword}@${shop}.myshopify.com/admin/api/2022-01/${resource}.json`
+//   // const apiUrl = `https://jsonplaceholder.typicode.com/todos`
+//   return axios(
+//     {
+//       method: 'get',
+//       url: apiUrl,
+    
+//       headers: {
+//         "content-type": "application/json, charset=utf-8",
+//         'X-Shopify-Access-Token': `${token}`,
+//         authorization : `${passsword}`, 
+//         // "Access-Control-Allow-Origin": "*", 
+//         // "Access-Control-Allow-Headers": "*"
+//       },
+
+//     })
+// }
 
 function App() {
 
+  useEffect(() => {
+    console.log("front");
+   axios({
+    method:'get', 
+    url:'/products', 
 
-
-    useEffect(()=>{
-
-
-
-   getAllCustomers()
-   .then((res)=>{
-
-     console.log(res.data);
    })
- 
+   .then((res)=>{
+      console.log(res.data["products"]);
+   })
+   .catch((e)=>{
+    console.log(e.message);
+   })
+  }, []);
 
-
-}, []);
-
-
-
-return (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/ShopifyApi/index.js
@@ -0,0 +1,48 @@
+const { default: axios } = require('axios');
+const express = require ('express');
+
+const app = express(express);
+const port = 5000;
+// console.log('server express');
+
+const shop = 'iti-ism';
+const version = "2022-07";
+const resource = "products";
+const token = "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";//iti-ism
+const apiKey = "f9435203660033c2fe73a34c23ffd4dd";// iti-ism
+const passsword = "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"//iti-ism
+
+
+
+app.use(async (req, res, next)=>{
+
+
+// console.log(data["data"]);
+    next();
+
+})
+app.get('/products',  (req, res)=>{
+    
+    getAllProducts()
+    .then((response)=>{
+        let products = response.data['products'];
+        console.log("*****************************************************************************************"
+        +"**********************************************************************************************");
+        console.log(products[0]);
+        res.json({
+            products:products,
+        })
+
+    })
+    .catch((e)=>{
+            console.error(e.response);
+            res.send('error')
+    })
+
+
+})
+
+app.listen(port, ()=>{
+
+    console.log(`listeneing to port ${port}`);
+})
